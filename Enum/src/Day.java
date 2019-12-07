3458dd3f7c97472b70de49951a5e9d22882a944a
Aditya Pardeshi <apardeshi@pdxinc.com>
2019-12-08 01:40:17 +0530

    commit first

---
+++ Enum/src/Day.java
@@ -0,0 +1,10 @@
+
+public enum Day {
+	SUNDAY("Sunday"),MONDAY("Monday");
+	private String day;
+	Day(String day)
+	{
+		this.day=day;
+	}
+
+}
---
+++ Enum/src/Main.java
@@ -0,0 +1,10 @@
+
+public class Main {
+	public static final String a1="Aditya";
+	public static final String a2="Pardeshi";
+	public static void main(String[] args) {
+		System.out.println(""+a1.hashCode());
+		System.out.println(""+Main.a1.hashCode());
+	}
+
+}
